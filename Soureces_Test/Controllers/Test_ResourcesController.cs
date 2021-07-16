33d9ca7acd327f6366611a97de10288c70a8bb0b
Gluttony-Meow <glutony0910@gmail.com>
2021-07-16 21:31:50 +0700

    Initial commit

---
+++ Soureces_Test/Controllers/Test_ResourcesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Soureces_Test.Controllers
+{
+    public class Test_ResourcesController : Controller
+    {
+        // GET: Test_Resources
+        public ActionResult Index()
+        {
+            String getLanguage = this.Request.UserLanguages[0];
+
+            try
+            {
+                string selectLanguage1 = this.Request.UserLanguages[1];
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            ViewBag.selectLangua = getLanguage;
+            if (getLanguage.Contains("en-AU"))
+            {
+                return View("Index.en-AU");
+            }
+            if (getLanguage.Contains("vn-VN"))
+            {
+                return View("Index.en-AU");
+            }
+            return View();
+        }
+    }
+}
---
+++ Soureces_Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Soureces_Test.Startup))]
+namespace Soureces_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
